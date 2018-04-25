d5b90d577dc29456ee5c4da8d04a01f65c5874dd
Jonny Nguyen <jonnyn@live.com>
2018-04-25 03:45:17 -0700

    Initial Commit
    Initial Commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Footer from './Footer'
+import avatar from '../assets/images/avatar.jpg'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header id="header">
+                <div className="inner">
+                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
+                    <h1>Hello! I am <strong>JONNY NGUYEN</strong><br/>A Web & Mobile App Developer<br/>AEM Full-Stack Developer</h1>
+                </div>
+                <Footer />
+            </header>
+        )
+    }
+}
+
+export default Header
